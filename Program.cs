9b5072db532711d8c5b7cc9f5d3515dce5bcb835
MikhailVinogradov224 <m89119750476@gmail.com>
2022-04-24 01:57:41 +0300

    done

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while(a>=i)
+{
+    double b = Math.Pow(i,3);
+    Console.Write($"{b} ");
+    i++;
+}
